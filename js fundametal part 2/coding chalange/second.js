6d055f83013c9ea28ef50cbf6d66480f368e28ce
Sumitray1 <sumitray206@gmail.com>
2022-11-07 20:15:58 +0545

    second challage complated sucessfully

--- js fundametal part 2/coding chalange/second.js
+++ js fundametal part 2/coding chalange/second.js
@@ -1,11 +1,20 @@
-'use strict';
+"use strict";
 
 
 function calcTip(billAmount) {
     if (billAmount > 50 && billAmount < 300) {
-        return (console.log(`Your tips is ${billAmount * 0.15} `));
+        return (billAmount * 0.15);
     } else {
-        return (console.log(`Your tips is ${billAmount * 0.2} `));
+        return (billAmount * 0.2);
     }
 }
-calcTip(150);
+const bills = [125, 555, 44];
+console.log("test data for given challange is ................");
+console.log(bills);
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+console.log("Tips of given three datal is ................");
+console.log(tips);
+const total = [calcTip(bills[0]) + bills[0], calcTip(bills[1]) + bills[1], calcTip(bills[2]) + bills[2]];
+console.log("total amount of given data i.e. tips + bill is ................");
+console.log(total);
+
