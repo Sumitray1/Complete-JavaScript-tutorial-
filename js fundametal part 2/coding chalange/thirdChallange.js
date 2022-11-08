eda32b07fed28b725f4963af3a622d7921bf4044
Sumitray1 <sumitray206@gmail.com>
2022-11-08 14:06:10 +0545

    challange 2 completed sucesfully

--- js fundametal part 2/coding chalange/thirdChallange.js
+++ js fundametal part 2/coding chalange/thirdChallange.js
@@ -10,4 +10,24 @@ const mark = {
     }
 
 }
+const miller = {
+    first: 'Miller',
+    last: 'Ray',
+    mass: Number(prompt("enter the mass of Miller ")),
+    height: Number(prompt("Enter the height of the Miller")),
+    BMi: function () {
+        return (this.mass / (this.height * this.height));
+    }
+
+
+}
+console.log(`inputs data for marks and miller are\n${mark}\n${miller}`);
+if (mark.BMi() > miller.BMi()) {
+    console.log(`${mark.first} ${mark.last} BMI(${mark.BMi()}) is higher than ${miller.first} ${miller.last} miller BMI(${miller.BMi()})`);
+
+} else {
+    console.log(`${mark.first} ${mark.last} BMI(${mark.BMi()}) is lower than ${mark.first} ${mark.last} miller BMI(${miller.BMi()})`);
+
+}
 console.log(`${mark.first} ${mark.last} weight ${mark.mass} kg is ${mark.height}m tall with ${mark.BMi()} BMI`);
+console.log(`${miller.first} ${miller.last} weight ${miller.mass} kg is ${miller.height}m tall with ${miller.BMi()} BMI`);
