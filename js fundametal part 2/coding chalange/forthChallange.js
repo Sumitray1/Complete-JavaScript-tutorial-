b0cb9e6e3615ca02345e653b991f4402a64960db
Sumitray1 <sumitray206@gmail.com>
2022-11-08 20:27:47 +0545

    error file

--- js fundametal part 2/coding chalange/forthChallange.js
+++ js fundametal part 2/coding chalange/forthChallange.js
@@ -9,9 +9,19 @@ function calcTip(bill) {
     }
     return tips;
 }
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+function totalAmout(tips, bills) {
+    let total = [];
+    for (let i = 0; i < tips.length; i++) {
+        total[i] = tips[i] + bills[i];
+
+    }
+    return total;
+}
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+tips.push(calcTip(bills));
+let totals = [];
+totals.push(totalAmout(tips, bills))
 console.log(`---------Billamounts are----------\n${bills} `);
-tips = calcTip(bills);
 console.log(`---------tips amount on bills amount are----------\n${tips} `);
+console.log(`--------- your total amount is------------------------\n ${totals}`);
