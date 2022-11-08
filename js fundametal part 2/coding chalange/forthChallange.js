b3aa931d5459ed629dc11fd5ea1c4a1445c31b81
Sumitray1 <sumitray206@gmail.com>
2022-11-08 22:21:08 +0545

    couldnt fix error

--- js fundametal part 2/coding chalange/forthChallange.js
+++ js fundametal part 2/coding chalange/forthChallange.js
@@ -1,27 +1,26 @@
-'use strict';
-
-
+"use strict";
 
 function calcTip(bill) {
-    let tips = [];
-    for (let j = 0; j < bill.length; j++) {
-        tips[j] = bill[j] * 0.2;
-    }
-    return tips;
+  let tips = [];
+  for (let j = 0; j < bill.length; j++) {
+    tips[j] = bill[j] * 0.2;
+  }
+  return tips;
 }
 function totalAmout(tips, bills) {
-    let total = [];
-    for (let i = 0; i < tips.length; i++) {
-        total[i] = tips[i] + bills[i];
-
-    }
-    return total;
+  let total = [];
+  for (let i = 0; i < tips.length; i++) {
+    total[i] = tips[i] + bills[i];
+  }
+  return total;
 }
 let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
 let tips = [];
 tips.push(calcTip(bills));
 let totals = [];
-totals.push(totalAmout(tips, bills))
+totals.push(totalAmout(tips, bills));
 console.log(`---------Billamounts are----------\n${bills} `);
 console.log(`---------tips amount on bills amount are----------\n${tips} `);
-console.log(`--------- your total amount is------------------------\n ${totals}`);
+console.log(
+  `--------- your total amount is------------------------\n ${totals}`
+);
