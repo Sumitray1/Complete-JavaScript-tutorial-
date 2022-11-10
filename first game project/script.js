c47764ef8af916a6443bbc608c633ac7deb6db32
Sumitray1 <sumitray206@gmail.com>
2022-11-10 19:15:28 +0545

    having fun working with project

---
+++ first game project/script.js
@@ -0,0 +1,16 @@
+'use strict';
+// // her "document.querySelector('.ClassName') is use for selection of class"
+// //where '.textContent' is used for selecting text on that class.
+// document.querySelector('.message').textContent = 'fuck gassing';
+// document.querySelector('.score').textContent = 0;
+// document.querySelector('.highscore').textContent = 100;
+// document.querySelector('.guess').value = 10;
+
+// secoond attempt for understanding
+// document.querySelector('.check').addEventListener('click', function () {
+//   document.querySelector('.message').textContent = 'fuck you..';
+
+//   document.querySelector('.number').textContent =
+//     document.querySelector('.guess').value;
+// });
+//understood
