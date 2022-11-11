a03b063a179e555a46e98591b85e0657e0a9b44b
Sumitray1 <sumitray206@gmail.com>
2022-11-11 11:16:09 +0545

    almost complete

--- Model project 2/script.js
+++ Model project 2/script.js
@@ -1,1 +1,21 @@
 'use strict';
+const modalHidden = document.querySelector('.hidden');
+const btnClose = document.querySelector('.close-modal');
+const overlayHidden = document.querySelector('.overlay');
+const BtnModal = document.querySelectorAll('.show-modal');
+console.log(BtnModal);
+let closeMsgBox = function () {
+  modalHidden.classList.add('hidden');
+  overlayHidden.classList.add('hidden');
+};
+
+for (let i = 0; i < BtnModal.length; i++) {
+  BtnModal[i].addEventListener('click', function () {
+    //first method call
+    // modalHidden.style.display = 'block';
+    //second method call
+    modalHidden.classList.remove('hidden');
+    overlayHidden.classList.remove('hidden');
+  });
+}
+btnClose.addEventListener('click', closeMsgBox);
