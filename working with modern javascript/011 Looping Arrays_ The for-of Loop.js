93792a72f1b8cb7d5aa0c51b078152a3ee06bf53
Sumitray1 <sumitray206@gmail.com>
2022-11-16 11:09:17 +0545

    ready for challange

--- working with modern javascript/011 Looping Arrays_ The for-of Loop.js
+++ working with modern javascript/011 Looping Arrays_ The for-of Loop.js
@@ -1,23 +1,23 @@
-'use strict';
-//perviously we need to print or use all the data from array we use  create loop but .....with new version of javascript.. we can do this by following this code---->
-//for example we need to print the data from this array we can use this--->
-const firsNames = [
-  'sumit',
-  'amit',
-  'partik',
-  'abhinay',
-  'abhi',
-  'computer',
-  'english',
-];
-//from this we can direclty print the values of array instead of creating loop
-for (let name of firsNames) {
-  console.log(name);
-}
-//if we want an array with index then we can do this --->
-for (const name of firsNames.entries()) {
-  console.log(name);
-}
-//if we want to create array within array with index do this --->
-console.log(...firsNames.entries());
-//this all for this  session second lesson starting soon
+// 'use strict';
+// //perviously we need to print or use all the data from array we use  create loop but .....with new version of javascript.. we can do this by following this code---->
+// //for example we need to print the data from this array we can use this--->
+// const firsNames = [
+//   'sumit',
+//   'amit',
+//   'partik',
+//   'abhinay',
+//   'abhi',
+//   'computer',
+//   'english',
+// ];
+// //from this we can direclty print the values of array instead of creating loop
+// for (let name of firsNames) {
+//   console.log(name);
+// }
+// //if we want an array with index then we can do this --->
+// for (const name of firsNames.entries()) {
+//   console.log(name);
+// }
+// //if we want to create array within array with index do this --->
+// console.log(...firsNames.entries());
+// //this all for this  session second lesson starting soon
--- working with modern javascript/Opteional chining.js
+++ working with modern javascript/Opteional chining.js
@@ -1,12 +1,12 @@
-'use strict';
-const restaurant = {
-  name: 'chinese',
-  location: 'patily',
-};
-if (restaurant.openingHours && restaurant.openingHours.mon) {
-  console.log(restaurant.openingHours.mon.open);
-}
-// WITH optional chaining
-console.log(restaurant.openingHours?.mon?.open);
-//previously it would show error if we use this as tradionation method ie.------>
-console.log(restaurant.openingHours.mon.open); //error massage will dispaly at console
+// 'use strict';
+// const restaurant = {
+//   name: 'chinese',
+//   location: 'patily',
+// };
+// if (restaurant.openingHours && restaurant.openingHours.mon) {
+//   console.log(restaurant.openingHours.mon.open);
+// }
+// // WITH optional chaining
+// console.log(restaurant.openingHours?.mon?.open);
+// //previously it would show error if we use this as tradionation method ie.------>
+// console.log(restaurant.openingHours.mon.open); //error massage will dispaly at console
