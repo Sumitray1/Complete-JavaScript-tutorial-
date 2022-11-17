03eef494ea5a98d02f680d164ec0d2f019f25592
Sumitray1 <sumitray206@gmail.com>
2022-11-17 21:56:25 +0545

    challange complated

--- working with modern javascript/cording chalange/cording_chalange2.js
+++ working with modern javascript/cording chalange/cording_chalange2.js
@@ -1,74 +1,74 @@
-'use strict';
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
-//challange 1---->
-console.log('------------challange 1------------');
-for (let i = 0; i < game.scored.length; i++) {
-  console.log(`Goal ${i + 1}: ${game.scored[i]}`);
-}
-console.log('--------------------another method---------');
-//another method
-for (const [i, player] of game.scored.entries()) {
-  console.log(`Goals ${i + 1}: ${player}`);
-}
-//challange 2---->
-console.log('--------------challange 2-----------');
-const oddsValue = Object.values(game.odds);
-let averageOddValue = 0;
-for (const value of oddsValue) {
-  averageOddValue += value;
-}
-console.log(`Average odds value =>  ${averageOddValue / oddsValue.length}`);
-//challange 3----->
-console.log('------------challange 3------------');
-const oddData = Object.values(game.odds);
-const keysName = [game.team1, 'draw', game.team2];
-console.log(keysName);
-for (let i = 0; i < oddData.length; i++) {
-  console.log(`Odds of ${keysName[i]}: ${oddData[i]}`);
-}
-//challange 3 mention method =>
-for (const [team, value] of Object.entries(game.odds)) {
-  const conditon = team === 'x' ? 'draw' : `victory${game[team]}`;
-  console.log(`odd of ${conditon} ${value}`);
-}
-//challenge 4------->
-console.log('--------------challenge 4-----------');
+// 'use strict';
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+// //challange 1---->
+// console.log('------------challange 1------------');
+// for (let i = 0; i < game.scored.length; i++) {
+//   console.log(`Goal ${i + 1}: ${game.scored[i]}`);
+// }
+// console.log('--------------------another method---------');
+// //another method
+// for (const [i, player] of game.scored.entries()) {
+//   console.log(`Goals ${i + 1}: ${player}`);
+// }
+// //challange 2---->
+// console.log('--------------challange 2-----------');
+// const oddsValue = Object.values(game.odds);
+// let averageOddValue = 0;
+// for (const value of oddsValue) {
+//   averageOddValue += value;
+// }
+// console.log(`Average odds value =>  ${averageOddValue / oddsValue.length}`);
+// //challange 3----->
+// console.log('------------challange 3------------');
+// const oddData = Object.values(game.odds);
+// const keysName = [game.team1, 'draw', game.team2];
+// console.log(keysName);
+// for (let i = 0; i < oddData.length; i++) {
+//   console.log(`Odds of ${keysName[i]}: ${oddData[i]}`);
+// }
+// //challange 3 mention method =>
+// for (const [team, value] of Object.entries(game.odds)) {
+//   const conditon = team === 'x' ? 'draw' : `victory${game[team]}`;
+//   console.log(`odd of ${conditon} ${value}`);
+// }
+// //challenge 4------->
+// console.log('--------------challenge 4-----------');
---
+++ working with modern javascript/cording chalange/cordingchallsnge-3.js
@@ -0,0 +1,41 @@
+'use strict';
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+//challange 1----->
+console.log('------------CHALLENGE 1------------');
+const events = new Set([...gameEvents.values()]);
+console.log(events);
+//challenge 2 -------------------->
+console.log('------------CHALLENGE 2------------');
+gameEvents.delete(64);
+console.log(gameEvents);
+//challenge 3 -------------------->
+console.log('------------CHALLENGE 3------------');
+console.log(`An event happened, on average, every ${
+  92 / gameEvents.size
+} minutes
+`);
+//Challange 4 ------------------->
+console.log('------------CHALLENGE 4------------');
+const halfTime = 45;
+for (const [keys, value] of gameEvents) {
+  if (keys <= halfTime) {
+    console.log(`[FIRST HALF] ${keys}: ${value}`);
+  } else {
+    console.log(`[SECOND HALF] ${keys}: ${value}`);
+  }
+}
+console.log(
+  '---------------------- CHALLENGE COMPLATED----------------------------'
+);
